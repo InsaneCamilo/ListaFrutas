847534a9d5f5711a47fcb57a71b6b1cab2fa833b
Taller 7-102 <Estudiante7102@talleresinpahu.edu>
2022-03-25 10:02:37 -0500

    szs

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+frutas = ["Kiwi","Sandia","Pera","Fresa","Mora","Guanabana","Mora","Manzana"]
+print(frutas[0])
+print(frutas[1])
+print(frutas[2])
+print(frutas[3])
+print(frutas[4])
+print(frutas[5])
+print(frutas[6])
+print(frutas[7])
